9cefadec73e05f00fc688c6b82f58c03c305408d
Cristofer Ramos <cramos-2023196@kinal.edu.gt>
2026-08-30 21:54:14 -0600

    feat: Se agrego la clase MainMenuController y se relizaro pequeños detaller en los FXML de Login y MainMenu

--- src/org/cristoferramos/system/controller/MainMenuController.java
+++ src/org/cristoferramos/system/controller/MainMenuController.java
@@ -4,10 +4,67 @@
  */
 package org.cristoferramos.system.controller;
 
+//import java.io.IOException;
+//import javafx.fxml.FXML;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.Menu;
+//import javafx.scene.control.MenuBar;
+//import javafx.stage.Stage;
+//import system.model.User;
+//import system.utils.SceneManager;
+
 /**
  *
- * @author sulma
+ * @author Cristofer Ramos
  */
-public class MainMenuController {
-    
+public class MainMenuController{
+//
+//    // Variables vinculadas al FXML (asegúrate que los fx:id coincidan)
+//    @FXML
+//    private MenuBar menuBarMain;
+//
+//    @FXML
+//    private Menu menuUser;
+//
+//    @FXML
+//    private Button btnLogOut;
+//
+//    @FXML
+//    private javafx.scene.layout.StackPane contentArea; // Área central vacía
+//
+//    private User usuarioActual;
+//
+//    /**
+//     * MÉTODO NUEVO: Se llama desde SceneManager para inyectar el usuario logueado.
+//     * Actualiza la interfaz con el nombre del usuario.
+//     */
+//    public void setUsuario(User usuario) {
+//        this.usuarioActual = usuario;
+//        if (usuario != null) {
+//            // Actualiza el texto del menú superior
+//            menuUser.setText("Usuario: " + usuario.getUsername());
+//        }
+//    }
+//
+//    /**
+//     * MÉTODO NUEVO: Acción del botón "Cerrar Sesión".
+//     * Limpia la sesión y devuelve al usuario a la pantalla de Login.
+//     */
+//    @FXML
+//    private void cerrarSesion() {
+//        this.usuarioActual = null; // Limpiar referencia en memoria
+//        
+//        try {
+//            // Obtenemos la ventana actual (Stage)
+//            Stage stage = (Stage) btnLogOut.getScene().getWindow();
+//            
+//            // Navegamos de vuelta al Login usando SceneManager
+//            SceneManager.cambiarEscena(stage, "LoginView");
+//            
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            System.err.println("Error crítico al cerrar sesión: " + e.getMessage());
+//        }
+//    }
+//    
 }
